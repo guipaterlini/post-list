730cc703738eb4e3633a3245c4bbd44e78231516
DLW <dlw@MacBook-Pro-de-DLW.local>
2023-04-29 13:47:27 -0400

    Feature: dados puxados com sucesso pela api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 "use strict";
-import "./lot.js";
+import "./posts.js";
--- src/posts.ts
+++ src/posts.ts
@@ -4,3 +4,16 @@ type Post = {
   title: string;
   body: string;
 };
+
+function getPosts(): Promise<Post[]> {
+  return fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+    res.json()
+  );
+}
+
+async function testPost() {
+  const data = await getPosts();
+  console.log(data);
+}
+
+testPost();
